ea2f395381c228c0a4f607ce2593159c4a190bfc
Jose DelaCruz <jose@delacruzhome.com>
2017-05-14 18:30:51 -0400

    Initial Commit

---
+++ AutoFixture.AutoMoq.WebApi/ApiControllerConventions.cs
@@ -0,0 +1,18 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+namespace AutoFixture.AutoMoq.ApiControllerConventions
+{
+
+    public class ApiControllerConventions : CompositeCustomization
+    {
+        public ApiControllerConventions()
+            : base(
+                new HttpRequestMessageCustomization(),
+                new ApiControllerCustomizationBase(),
+                new AutoMoqCustomization())
+        {
+        }
+    }
+
+}
---
+++ AutoFixture.AutoMoq.WebApi/HttpRequestMessageCustomization.cs
@@ -0,0 +1,19 @@
+﻿using Ploeh.AutoFixture;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace AutoFixture.AutoMoq.ApiControllerConventions
+{
+    public class HttpRequestMessageCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<HttpRequestMessage>(c => c
+            .Without(x => x.Content)
+            .Do(x => x.Properties[HttpPropertyKeys.HttpConfigurationKey] =
+                new HttpConfiguration()));
+
+        }
+    }
+}
